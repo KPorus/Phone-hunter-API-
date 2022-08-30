655b28e07f0d99817a76e110169b5825b8772d58
KPorus <designerfardin2021@gmail.com>
2022-08-30 09:06:06 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+let loadPhone = (search) =>{
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`)
+    .then(res => res.json())
+    .then(data => display(data.data))
+}
+
+let display = phone =>{
+    let phoneContainer = document.getElementById(`phone-container`);
+    phoneContainer.textContent = " ";
+    phone.forEach(phones => {
+        let phonebody = document.createElement(`div`);
+        phonebody.classList.add(`col`);
+        phonebody.innerHTML = `
+        <div class="card bg-dark text-white">
+          <img src="${phones.image}" class="card-img-top img-fluid w-100" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Phone name: ${phones.phone_name}</h5>
+            <h5 class="card-title">Brand: ${phones.brand}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+        `
+        phoneContainer.appendChild(phonebody);
+    });
+}
+
+document.getElementById(`search`).addEventListener(`click`,function(){
+    let phoneSearch = document.getElementById(`phone-search`).value;
+    let search =phoneSearch;
+    loadPhone(search);
+})
+loadPhone();
